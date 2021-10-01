482ff2429c31255f84ef963cf1bba4a21edda400
bong <qhdgns613@gmail.com>
2021-10-01 18:12:57 +0900

    충돌~

--- src/project/dao/ProjectDAO.java
+++ src/project/dao/ProjectDAO.java
@@ -1,5 +1,15 @@
 package project.dao;
 
+
+
 public class ProjectDAO {
-	
+
+	static {
+		try {
+			Class.forName("oracle.jdbc.OracleDriver");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
