be07356577aac5705556831a0a47add63d119609
kyuho <dlrbgh9303@gmail.com>
2021-10-01 18:13:42 +0900

    test

--- src/project/dao/ProjectDAO.java
+++ src/project/dao/ProjectDAO.java
@@ -2,4 +2,15 @@ package project.dao;
 
 public class ProjectDAO {
 	
+	
+	static {
+		try {
+			Class.forName("Oracle.jdbc.OracleDriver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	
 }
