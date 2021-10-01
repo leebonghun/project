9c90d8fa5eca3b694bb8d414c92111848a532926
Juan <cabriogt@gmail.com>
2021-10-01 18:13:50 +0900

    edit

--- src/project/dao/ProjectDAO.java
+++ src/project/dao/ProjectDAO.java
@@ -2,4 +2,13 @@ package project.dao;
 
 public class ProjectDAO {
 	
+	static {
+		try {
+			Class.forName("oracle.jdbc.OracleDriver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
 }
