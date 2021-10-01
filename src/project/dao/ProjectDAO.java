79f9c9cc36696de03350be45efd443066ff083e0
Ella <yantle@naver.com>
2021-10-01 18:13:11 +0900

    드라이버 로드

--- src/project/dao/ProjectDAO.java
+++ src/project/dao/ProjectDAO.java
@@ -2,4 +2,12 @@ package project.dao;
 
 public class ProjectDAO {
 	
+	static {
+		try {
+			Class.forName("oracle.jdbc.oracledriver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+	}
 }
