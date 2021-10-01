31ff35273c60e65f29544114bd861a434f6251c2
ahreum <dnflsms357@gmail.com>
2021-10-01 18:13:31 +0900

    드라이버 로드

--- src/project/dao/ProjectDAO.java
+++ src/project/dao/ProjectDAO.java
@@ -1,5 +1,13 @@
 package project.dao;
 
 public class ProjectDAO {
-	
+	static {
+		try {
+			Class.forName("oracle.jdbc.OracleDriver");
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
 }
