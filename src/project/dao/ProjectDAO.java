50d5dd61db0c0cc9310f436d1e150f18d89b3c38
mykim <minyk112@gmail.com>
2021-10-05 10:11:23 +0900

    ㅁㄴ

--- src/project/dao/ProjectDAO.java
+++ src/project/dao/ProjectDAO.java
@@ -4,40 +4,28 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 
 public class ProjectDAO {
-	
-<<<<<<< HEAD
-=======
-	
->>>>>>> branch 'master' of https://github.com/leebonghun/project.git
+
 	static {
 		Connection con = null;
 		PreparedStatement pstmt = null;
-		
+
 		try {
-<<<<<<< HEAD
 			Class.forName("oracle.jdbc.oracledriver");
-			
-		} catch (Exception e) {
-=======
-			Class.forName("Oracle.jdbc.OracleDriver");
+
 		} catch (ClassNotFoundException e) {
->>>>>>> branch 'master' of https://github.com/leebonghun/project.git
 			e.printStackTrace();
 		} finally {
 			try {
-				
+
 			} catch (Exception e2) {
 				e2.printStackTrace();
 			}
 		}
 
-
 		System.out.println("안녕하세요");
 
-
-
 	}
-	
-	
-
+	public void temp() {
+		System.out.println("안녕하세요~");
+	}
 }
